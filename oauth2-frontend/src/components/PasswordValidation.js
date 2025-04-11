336547155cf1acad5509fab314eff30a2959ce35
HuseyinK0r0glu <korogluh27@gmail.com>
2025-04-11 12:31:31 +0300

    small change to validation

--- oauth2-frontend/src/components/PasswordValidation.js
+++ oauth2-frontend/src/components/PasswordValidation.js
@@ -37,7 +37,7 @@ const PasswordValidation = ({password,setIsValid,setPasswordRules}) => {
         setIsValid(isPasswordValid);
         setPasswordRules(newRules);
       };
-
+      
 
       useEffect(() => {
         checkPasswordStrength();
