461cdb92db5123116aaf4bff72524597d9ab80ec
HuseyinK0r0glu <korogluh27@gmail.com>
2025-04-25 09:08:46 +0300

    profile picture added to user page

--- oauth2-frontend/src/screens/general/UserPage.js
+++ oauth2-frontend/src/screens/general/UserPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import defaultUserImage from "../../assets/defaultUserImage.jpeg"
+import { useProfileImage } from "../../hooks/useProfileImage";
 
 const UserPage = () => {
     
@@ -11,38 +12,48 @@ const UserPage = () => {
 
     const users = JSON.parse(localStorage.getItem("users")) || [];
     
-    if(!users){
-        return <p>Users is empty</p>
-    }
-
     const user = users.find(u => u.id.toString() === id);
 
-    if(!user){
-        return <p>User not found</p>
-    }
+    const imageUrl = useProfileImage({user : user});
 
     return(
         <div className="container text-center mt-5">
             <h1 className="mb-4">User Profile</h1>
             <div className="card p-4 shadow-sm" style={{ maxWidth: "400px", margin: "auto" }}>
-                {user.picture 
-                ? 
-                <img 
-                    src={user.picture} 
-                    alt="Profile" 
-                    className="rounded-circle border mb-3"
-                    width="100" 
-                    height="100" 
-                />
-                :
-                <img 
-                    src={defaultUserImage} 
-                    alt="Profile" 
-                    className="rounded-circle border mb-3"
-                    width="100" 
-                    height="100" 
-                />
-                }
+                {user.local_picture
+                    ? 
+                    <img 
+                        src={imageUrl} 
+                        alt="Profile" 
+                        className="rounded-circle border mb-3"
+                        style={{
+                        width: "100px",
+                        height: "100px",
+                        objectFit: "cover",
+                        }}
+                    />
+                    :
+                    user.picture 
+                    ?
+                    <img 
+                        src={user.picture} 
+                        alt="Profile" 
+                        className="rounded-circle border mb-3"
+                        width="100" 
+                        height="100" 
+                    />
+                    :
+                    <img 
+                        src={defaultUserImage} 
+                        alt="Profile" 
+                        className="rounded-circle border mb-3"
+                        style={{
+                        width: "100px",
+                        height: "100px",
+                        objectFit: "cover",
+                        }}
+                    />
+                    }
                 <h2 className="h4">{user.username}</h2>
                 <p className="text-muted">Email: {user.email}</p>
             </div>
