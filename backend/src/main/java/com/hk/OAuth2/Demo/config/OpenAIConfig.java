723341476b3d316cc5010815066ba1829e25e25f
HuseyinK0r0glu <korogluh27@gmail.com>
2025-05-14 11:52:47 +0300

    api key and config added

---
+++ backend/src/main/java/com/hk/OAuth2/Demo/config/OpenAIConfig.java
@@ -0,0 +1,14 @@
+package com.hk.OAuth2.Demo.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class OpenAIConfig {
+
+    @Value("${openai.api.key}")
+    private String apiKey;
+
+}
