d20f8a376805095cbf7e8994957b1bc853ebf139
HuseyinKoroglu <korogluh27@gmail.com>
2025-08-06 15:09:24 +0300

    admin properties removed to .env file

--- backend/src/main/java/com/hk/OAuth2/Demo/admin/AdminInitializer.java
+++ backend/src/main/java/com/hk/OAuth2/Demo/admin/AdminInitializer.java
@@ -2,6 +2,7 @@ package com.hk.OAuth2.Demo.admin;
 
 import com.hk.OAuth2.Demo.entity.User;
 import com.hk.OAuth2.Demo.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,15 @@ import java.util.Arrays;
 
 @Component
 public class AdminInitializer implements CommandLineRunner {
+
+    @Value("${admin.username}")
+    private String adminUsername;
+
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    @Value("${admin.password}")
+    private String adminPassword;
 
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
@@ -23,10 +33,10 @@ public class AdminInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
         if(!userService.existsByUserName("admin")){
             User admin = new User();
-            admin.setUsername("admin");
-            admin.setPassword(passwordEncoder.encode("admin"));
+            admin.setUsername(adminUsername);
+            admin.setPassword(passwordEncoder.encode(adminPassword));
             admin.setVerified(1);
-            admin.setEmail("admin@hk.com");
+            admin.setEmail(adminEmail);
             admin.setRoles(Arrays.asList("ROLE_ADMIN", "ROLE_USER"));
             admin.setFailedAttempts(0);
             userService.save(admin);
